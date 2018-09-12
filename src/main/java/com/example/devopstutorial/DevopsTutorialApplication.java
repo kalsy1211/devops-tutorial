8f0f2e7e5dedd8824081cbce713bfb8a0f6da30b
saurabh kalra <kalsy1211@gmail.com>
2018-09-11 22:42:55 -0700

    Initial feature commit

---
+++ src/main/java/com/example/devopstutorial/DevopsTutorialApplication.java
@@ -0,0 +1,12 @@
+package com.example.devopstutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsTutorialApplication.class, args);
+	}
+}
